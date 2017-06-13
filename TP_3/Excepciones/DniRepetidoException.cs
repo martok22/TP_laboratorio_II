c10b7f80906ea12a238cdf6372d3ffaeeb47a5d1
Martín González <martok22@users.noreply.github.com>
2017-06-13 03:43:36 -0300

    Add files via upload

---
+++ TP_3/Excepciones/DniRepetidoException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class DniInvalidoException : Exception
+    {
+        private static string mensajeBase = "Dni repetido";
+
+        public DniInvalidoException() : base() { }
+
+        public DniInvalidoException(string mensaje) : base(mensaje) { }
+
+        public DniInvalidoException(Exception e) : base(mensajeBase,e){ }
+
+        public DniInvalidoException(string mensaje, Exception e):this(mensaje) { }
+
+    }
+}
---
+++ TP_3/Excepciones/NacionalidadInvalidaException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class NacionalidadInvalidaException : Exception
+    {
+        public NacionalidadInvalidaException() : base("La nacionalidad no se condice con el dni") { }
+
+        public NacionalidadInvalidaException(string mensaje) : base(mensaje){ }
+
+    }
+}
